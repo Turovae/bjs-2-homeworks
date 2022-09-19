65b976ecaea348a7ab898fb4c3d2941485ade77b
TurovaE <ekaterina.turova@mail.ru>
2022-09-19 23:21:38 +0700

    Realisation task 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,38 @@
 function Student(name, gender, age) {
     // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+  this.subject = subjectName;
 }
 
 // ваш код для остальных методов
+
+Student.prototype.addMark = function (mark) {
+  if (!this.marks) {
+    this.marks = new Array();
+  }
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...arg) {
+  if (!this.marks) {
+    this.marks = new Array();
+  }
+  this.marks.push(...arg);
+}
+
+Student.prototype.getAverage = function() {
+  return this.marks.reduce((accum, elem) => accum + elem, 0) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
+}
